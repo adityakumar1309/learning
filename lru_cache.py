0fdf9183cc8782567d9639c9705d93877dfa12ec
xyz <akumar@Ban-1akumar-m-2.local>
2018-06-13 03:53:41 +0530

    bug fix when same element comes which is already most recent then dont do anything check is added , now it works for Pages: [2, 6, 2, 1, 6, 4, 4, 3]

--- lru_cache.py
+++ lru_cache.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self, value, prev=None, next=None):
+    def __init__(self, value, prev=None, nxt=None):
         self.value = value
         self.prev = prev
-        self.next = next
+        self.nxt = nxt
 
 class DoubleLinkedList:
     def __init__(self):
@@ -14,24 +14,6 @@ class LRUCache(object):
         self.cache_table = {}
         self.maxsize = maxsize
 
-    def is_full(self):
-        if self.get_size() >= self.maxsize:
-            return True
-        return False
-
-    def get_size(self):
-        size = 0
-        if self.cache_list.root is None:
-            return size
-        else:
-            size = 1
-            size = 1
-            item = self.cache_list.root.next
-            while item != self.cache_list.root:
-                size = size + 1
-                item = item.next
-            return size
-
     def save_page(self, key):
         if self.cache_table.get(key, None) != None:
             # already in cache
@@ -39,24 +21,29 @@ class LRUCache(object):
         else:
             # not in cache
             desired_node = Node(key)
-            self.cache_table[key] = desired_node
-            if self.is_full():
+            if len(self.cache_table) >= self.maxsize:
+                #if cache is full
                 self.delete_oldest()
+            self.cache_table[key] = desired_node
         self.mark_most_recent(desired_node)
         return desired_node.value
 
     def delete_oldest(self):
-        oldest = self.cache_list.root.previous
-        oldest.previous.next = self.cache_list.root
-        self.cache_list.root.previous = oldest.previous
+        oldest = self.cache_list.root.prev
+        oldest.prev.nxt = self.cache_list.root
+        self.cache_list.root.prev = oldest.prev
         oldest = None
         
     def mark_most_recent(self, recent_node):
         if self.cache_list.root == None:
             # list is empty
-            recent_node.previous = recent_node.next = recent_node
+            recent_node.nxt = recent_node
+            recent_node.prev = recent_node
             self.cache_list.root = recent_node
-        elif recent_node.next != None and recent_node.previous != None:
+        elif recent_node == self.cache_list.root:
+            #if recent node is already the most recent then do nothing
+            return
+        elif recent_node.nxt != None and recent_node.prev != None:
             # node is already in list somewhere
             self.remove_from_middle(recent_node)
             self.add_to_head(recent_node)
@@ -66,34 +53,37 @@ class LRUCache(object):
 
     def add_to_head(self, new_root):
         old_root = self.cache_list.root
-        tail = old_root.previous
-        tail.next = new_root
-        new_root.previous = tail
-        old_root.previous = new_root
-        new_root.next = old_root
+        tail = old_root.prev
+        tail.nxt = new_root
+        new_root.prev = tail
+        old_root.prev = new_root
+        new_root.nxt = old_root
         self.cache_list.root = new_root
 
     def remove_from_middle(self, new_root):
-        new_root.next.previous = new_root.previous
-        new_root.previous.next = new_root.next
+        new_root.nxt.prev = new_root.prev
+        new_root.prev.nxt = new_root.nxt
 
     def get_cached_results(self):
         l = []
         item = self.cache_list.root
         if item == None:
             return []
-        while item.next != self.cache_list.root:
+        while item.nxt != self.cache_list.root:
             l.append(item.value)
-            item = item.next
+            item = item.nxt
         l.append(item.value)
         return l
 
 if __name__ == "__main__":
-    pages = [2, 6, 2, 1, 6, 4]
+    pages = [2, 6, 2, 1, 6, 4, 4, 3]
     print "Pages: %s"%pages
     cache_obj = LRUCache(maxsize=3)
     print cache_obj.get_cached_results()
     for page in pages:
+        if page == 4:
+            pass
+            #import pdb;pdb.set_trace()
         prev = cache_obj.get_cached_results()
         cache_obj.save_page(page)
         print "curr: %s, add %s, new: %s"%(prev, page, cache_obj.get_cached_results())
