4768787c6d8d3d07261c4844060fb84d33cbd854
adityakumar1309 <40203957+adityakumar1309@users.noreply.github.com>
2018-10-25 01:10:20 +0530

    Add files via upload

--- algorithm_data_structure/graph/minimum-time-required-so-that-all-oranges-become-rotten.py
+++ algorithm_data_structure/graph/minimum-time-required-so-that-all-oranges-become-rotten.py
@@ -1,29 +1,3 @@
-"""
-Minimum time required to rot all oranges
-Given a matrix of dimension m*n where each cell in the matrix can have values 0, 1 or 2 which has the following meaning:
-0: Empty cell
-
-1: Cells have fresh oranges
-
-2: Cells have rotten oranges 
-So we have to determine what is the minimum time required so that all the oranges become rotten. A rotten orange at index [i,j] can rot other fresh orange at indexes [i-1,j], [i+1,j], [i,j-1], [i,j+1] (up, down, left and right). If it is impossible to rot every orange then simply return -1.
-
-Examples:
-
-Input:  arr[][C] = { {2, 1, 0, 2, 1},
-                     {1, 0, 1, 2, 1},
-                     {1, 0, 0, 2, 1}};
-Output:
-All oranges can become rotten in 2 time frames.
-
-
-Input:  arr[][C] = { {2, 1, 0, 2, 1},
-                     {0, 0, 1, 2, 1},
-                     {1, 0, 0, 2, 1}};
-Output:
-All oranges cannot be rotten.
-"""
-
 from collections import deque
 
 def min_time_frame(arr):
@@ -51,10 +25,10 @@ def min_time_frame(arr):
             continue
 
         row, col = position
-        row_comb = [-1, -1, -1,  0, 0,  1, 1, 1]
-        col_comb = [-1,  0,  1, -1, 1, -1, 0, 1]
+        row_comb = [-1,  0, 0, 1]
+        col_comb = [ 0, -1, 1, 0]
 
-        for k in range(8):
+        for k in range(4):
             x_index, y_index = row_comb[k], col_comb[k]
             if row+x_index < MAX_ROW and row+x_index >= 0 and col+y_index < MAX_COL and col+y_index >= 0 and arr[row+x_index][col+y_index] == 1:
                 arr[row+x_index][col+y_index] = 2
